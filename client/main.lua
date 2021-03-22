e4a96ee5f4616d0596d8fba24aae54f9c553c5b1
FlawwsX <55849405+FlawwsX@users.noreply.github.com>
2021-03-22 17:16:59 +0000

    change(client/main): use backticks over native

--- client/main.lua
+++ client/main.lua
@@ -34,14 +34,14 @@ end)
 -- radio submix
 local radioEffectId = CreateAudioSubmix('Radio')
 SetAudioSubmixEffectRadioFx(radioEffectId, 0)
-SetAudioSubmixEffectParamInt(radioEffectId, 0, GetHashKey('default'), 1)
+SetAudioSubmixEffectParamInt(radioEffectId, 0, `default`, 1)
 AddAudioSubmixOutput(radioEffectId, 0)
 
 local phoneEffectId = CreateAudioSubmix('Phone')
 SetAudioSubmixEffectRadioFx(phoneEffectId, 1)
-SetAudioSubmixEffectParamInt(phoneEffectId, 1, GetHashKey('default'), 1)
-SetAudioSubmixEffectParamFloat(phoneEffectId, 1, GetHashKey('freq_low'), 700.0)
-SetAudioSubmixEffectParamFloat(phoneEffectId, 1, GetHashKey('freq_hi'), 15000.0)
+SetAudioSubmixEffectParamInt(phoneEffectId, 1, `default`, 1)
+SetAudioSubmixEffectParamFloat(phoneEffectId, 1, `freq_low`, 700.0)
+SetAudioSubmixEffectParamFloat(phoneEffectId, 1, `freq_hi`, 15000.0)
 AddAudioSubmixOutput(phoneEffectId, 1)
 
 local submixFunctions = {
