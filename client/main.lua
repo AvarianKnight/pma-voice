4754a4dfff1e897f14df6d7d0e06c7fbfbaa20d5
Avarian Knight <dillskaggs@gmail.com>
2021-09-14 11:06:53 -0500

    fix(client/main): fix logic being wrong

--- client/main.lua
+++ client/main.lua
@@ -409,7 +409,7 @@ AddEventHandler('onClientResourceStart', function(resource)
 
 	-- Some people modify pma-voice and mess up the resource Kvp, which means that if someone
 	-- joins another server that has pma-voice, it will error out, this will catch and fix the kvp.
-	local errored = pcall(function() 
+	local success = pcall(function() 
 		local micClicksKvp = GetResourceKvpString('pma-voice_enableMicClicks')
 		if not micClicksKvp then
 			SetResourceKvp('pma-voice_enableMicClicks', tostring(true))
@@ -421,7 +421,7 @@ AddEventHandler('onClientResourceStart', function(resource)
 		end
 	end)
 
-	if errored then
+	if not success then
 		logger.warn('Failed to load resource Kvp, likely was inapproparielty modified by another server, resetting the Kvp.')
 		SetResourceKvp('pma-voice_enableMicClicks', tostring(true))
 		micClicks = 'true'
