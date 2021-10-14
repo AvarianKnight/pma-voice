bfa919234eb84e7df810f15a63a9e784b19d0832
Avarian Knight <dillskaggs@gmail.com>
2021-10-14 16:56:22 -0500

    fix(events): fix wrong using debug instead of info

--- client/events.lua
+++ client/events.lua
@@ -1,12 +1,12 @@
 AddEventHandler('mumbleConnected', function(address, isReconnecting)
-	logger.debug('Connected to mumble server with address of %s, is this a reconnect %s', GetConvarInt('voice_hideEndpoints', 1) == 1 and 'HIDDEN' or address, isReconnecting)
+	logger.info('Connected to mumble server with address of %s, is this a reconnect %s', GetConvarInt('voice_hideEndpoints', 1) == 1 and 'HIDDEN' or address, isReconnecting)
 	-- don't try to set channel instantly, we're still getting data.
 	Wait(1000)
 	addNearybyPlayers()
 end)
 
 AddEventHandler('mumbleDisconnected', function(address)
-	logger.debug('Disconnected from mumble server with address of %s', GetConvarInt('voice_hideEndpoints', 1) == 1 and 'HIDDEN' or address)
+	logger.info('Disconnected from mumble server with address of %s', GetConvarInt('voice_hideEndpoints', 1) == 1 and 'HIDDEN' or address)
 end)
 
 -- TODO: Convert the last Cfg to a Convar, while still keeping it simple.
