e85125fb90680fd4dbb0f127afa842ca45f8c0ef
Avarian Knight <dillskaggs@gmail.com>
2021-11-06 16:46:31 -0500

    fix(proximity): remove players from target if they're not in the proper channel

--- client/init/proximity.lua
+++ client/init/proximity.lua
@@ -18,18 +18,16 @@ function addNearbyPlayers()
 
 		local ped = GetPlayerPed(ply)
 		local isTarget = currentVoiceTargets[serverId]
-		if #(coords - GetEntityCoords(ped)) < distance then
-			local voiceChannel = MumbleGetVoiceChannelFromServerId(serverId)
+		local voiceChannel = MumbleGetVoiceChannelFromServerId(serverId)
+		if #(coords - GetEntityCoords(ped)) < distance and voiceChannel == serverId then
 			if isTarget then goto skip_loop end
-			if voiceChannel ~= serverId then
-				logger.warn("%s isn't set the right voice channel, got %s expected %s", serverId, voiceChannel, serverId)
-				goto skip_loop
-			end
-
 			logger.info('Added %s as a voice target', serverId)
 			MumbleAddVoiceTargetChannel(voiceTarget, serverId)
 			currentVoiceTargets[serverId] = true
 		elseif isTarget then
+			if voiceChannel ~= serverId then
+				logger.warn("%s isn't set the right voice channel, got %s expected %s, removing them as target", serverId, voiceChannel, serverId)
+			end
 			logger.info('Removed %s from voice targets', serverId)
 			MumbleRemoveVoiceTargetChannel(voiceTarget, serverId)
 			currentVoiceTargets[serverId] = nil
