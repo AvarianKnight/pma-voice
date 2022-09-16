49373feb2a689c9971e546c30e16e468089d966f
Gamzky <mike.vennix@gmail.com>
2022-09-16 15:53:30 +0200

    Make indentations consistent

--- server/module/phone.lua
+++ server/module/phone.lua
@@ -2,35 +2,35 @@
 ---@param source number the player to remove from the call
 ---@param callChannel number the call channel to remove them from
 function removePlayerFromCall(source, callChannel)
-    logger.verbose('[call] Removed %s from call %s', source, callChannel)
+	logger.verbose('[call] Removed %s from call %s', source, callChannel)
 
-    callData[callChannel] = callData[callChannel] or {}
-    for player, _ in pairs(callData[callChannel]) do
-        TriggerClientEvent('pma-voice:removePlayerFromCall', player, source)
-    end
-    callData[callChannel][source] = nil
-    voiceData[source] = voiceData[source] or defaultTable(source)
-    voiceData[source].call = 0
+	callData[callChannel] = callData[callChannel] or {}
+	for player, _ in pairs(callData[callChannel]) do
+		TriggerClientEvent('pma-voice:removePlayerFromCall', player, source)
+	end
+	callData[callChannel][source] = nil
+	voiceData[source] = voiceData[source] or defaultTable(source)
+	voiceData[source].call = 0
 end
 
 --- adds a player to a call
 ---@param source number the player to add to the call 
 ---@param callChannel number the call channel to add them to
 function addPlayerToCall(source, callChannel)
-    logger.verbose('[call] Added %s to call %s', source, callChannel)
-    -- check if the channel exists, if it does set the varaible to it
-    -- if not create it (basically if not callData make callData)
-    callData[callChannel] = callData[callChannel] or {}
-    for player, _ in pairs(callData[callChannel]) do
-        -- don't need to send to the source because they're about to get sync'd!
-        if player ~= source then
-            TriggerClientEvent('pma-voice:addPlayerToCall', player, source)
-        end
-    end
-    callData[callChannel][source] = false
-    voiceData[source] = voiceData[source] or defaultTable(source)
-    voiceData[source].call = callChannel
-    TriggerClientEvent('pma-voice:syncCallData', source, callData[callChannel])
+	logger.verbose('[call] Added %s to call %s', source, callChannel)
+	-- check if the channel exists, if it does set the varaible to it
+	-- if not create it (basically if not callData make callData)
+	callData[callChannel] = callData[callChannel] or {}
+	for player, _ in pairs(callData[callChannel]) do
+		-- don't need to send to the source because they're about to get sync'd!
+		if player ~= source then
+			TriggerClientEvent('pma-voice:addPlayerToCall', player, source)
+		end
+	end
+	callData[callChannel][source] = false
+	voiceData[source] = voiceData[source] or defaultTable(source)
+	voiceData[source].call = callChannel
+	TriggerClientEvent('pma-voice:syncCallData', source, callData[callChannel])
 end
 
 --- set the players call channel
@@ -38,11 +38,11 @@ end
 ---@param _callChannel number the channel to set the player to (or 0 to remove them from any call channel)
 function setPlayerCall(source, _callChannel)
 	if GetConvarInt('voice_enableCalls', 1) ~= 1 then return end
-    voiceData[source] = voiceData[source] or defaultTable(source)
-    local isResource = GetInvokingResource()
-    local plyVoice = voiceData[source]
-    local callChannel = tonumber(_callChannel)
-    if not callChannel then
+	voiceData[source] = voiceData[source] or defaultTable(source)
+	local isResource = GetInvokingResource()
+	local plyVoice = voiceData[source]
+	local callChannel = tonumber(_callChannel)
+	if not callChannel then
 		-- only full error if its sent from another server-side resource
 		if isResource then
 			error(("'callChannel' expected 'number', got: %s"):format(type(_callChannel)))
@@ -56,39 +56,39 @@ function setPlayerCall(source, _callChannel)
 		TriggerClientEvent('pma-voice:clSetPlayerCall', source, callChannel)
 	end
 
-    Player(source).state.callChannel = callChannel
+	Player(source).state.callChannel = callChannel
 
-    if callChannel ~= 0 and plyVoice.call == 0 then
-        addPlayerToCall(source, callChannel)
-    elseif callChannel == 0 then
-        removePlayerFromCall(source, plyVoice.call)
-    elseif plyVoice.call > 0 then
-        removePlayerFromCall(source, plyVoice.call)
-        addPlayerToCall(source, callChannel)
-    end
+	if callChannel ~= 0 and plyVoice.call == 0 then
+		addPlayerToCall(source, callChannel)
+	elseif callChannel == 0 then
+		removePlayerFromCall(source, plyVoice.call)
+	elseif plyVoice.call > 0 then
+		removePlayerFromCall(source, plyVoice.call)
+		addPlayerToCall(source, callChannel)
+	end
 end
 exports('setPlayerCall', setPlayerCall)
 
 RegisterNetEvent('pma-voice:setPlayerCall', function(callChannel)
-    setPlayerCall(source, callChannel)
+	setPlayerCall(source, callChannel)
 end)
 
 function setTalkingOnCall(talking)
 	if GetConvarInt('voice_enableCalls', 1) ~= 1 then return end
-    local source = source
-    voiceData[source] = voiceData[source] or defaultTable(source)
-    local plyVoice = voiceData[source]
-    local callTbl = callData[plyVoice.call]
-    if callTbl then
-        logger.verbose('[call] %s %s talking in call %s', source, talking and 'started' or 'stopped', plyVoice.call)
-        for player, _ in pairs(callTbl) do
-            if player ~= source then
-                logger.verbose('[call] Sending event to %s to tell them that %s is talking', player, source)
-                TriggerClientEvent('pma-voice:setTalkingOnCall', player, source, talking)
-            end
-        end
-    else
-        logger.verbose('[call] %s tried to talk in call %s, but it doesnt exist.', source, plyVoice.call)
-    end
+	local source = source
+	voiceData[source] = voiceData[source] or defaultTable(source)
+	local plyVoice = voiceData[source]
+	local callTbl = callData[plyVoice.call]
+	if callTbl then
+		logger.verbose('[call] %s %s talking in call %s', source, talking and 'started' or 'stopped', plyVoice.call)
+		for player, _ in pairs(callTbl) do
+			if player ~= source then
+				logger.verbose('[call] Sending event to %s to tell them that %s is talking', player, source)
+				TriggerClientEvent('pma-voice:setTalkingOnCall', player, source, talking)
+			end
+		end
+	else
+		logger.verbose('[call] %s tried to talk in call %s, but it doesnt exist.', source, plyVoice.call)
+	end
 end
 RegisterNetEvent('pma-voice:setTalkingOnCall', setTalkingOnCall)
--- shared.lua
+++ shared.lua
@@ -70,26 +70,26 @@ function tPrint(tbl, indent)
 end
 
 local function types(args)
-    local argType = type(args[1])
-    for i = 2, #args do
-        local arg = args[i]
-        if argType == arg then
-            return true, argType
-        end
-    end
-    return false, argType
+	local argType = type(args[1])
+	for i = 2, #args do
+		local arg = args[i]
+		if argType == arg then
+			return true, argType
+		end
+	end
+	return false, argType
 end
 
 --- does a type check and errors if an invalid type is sent
 ---@param ... table a table with the variable being the first argument and the expected type being the second 
 function type_check(...)
-    local vars = {...}
-    for i = 1, #vars do
-        local var = vars[i]
-        local matchesType, varType = types(var)
-        if not matchesType then
-            table.remove(var, 1)
-            error(("Invalid type sent to argument #%s, expected %s, got %s"):format(i, table.concat(var, "|"), varType))
-        end
-    end
+	local vars = {...}
+	for i = 1, #vars do
+		local var = vars[i]
+		local matchesType, varType = types(var)
+		if not matchesType then
+			table.remove(var, 1)
+			error(("Invalid type sent to argument #%s, expected %s, got %s"):format(i, table.concat(var, "|"), varType))
+		end
+	end
 end
