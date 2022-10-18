bb022f5f8f4024b8828242e0257ee72a4f40753b
Mustafa Burak Güneş <54374780+TGIANN@users.noreply.github.com>
2022-10-18 23:33:19 +0300

    Character looks at someone while talking

--- client/init/main.lua
+++ client/init/main.lua
@@ -298,3 +298,23 @@ if gameVersion == 'redm' then
 		end
 	end)
 end
+
+if gameVersion == 'fivem' then
+	CreateThread(function()
+		while true do
+			Wait(500)
+			local onlinePlayers = GetActivePlayers()
+			local playerPed = PlayerPedId()
+			for i=1, #onlinePlayers do
+				if onlinePlayers[i] ~= PlayerId() and NetworkIsPlayerActive(onlinePlayers[i]) then
+					if MumbleIsPlayerTalking(onlinePlayers[i]) then
+						local targetPed = GetPlayerPed(onlinePlayers[i])
+						if #(GetEntityCoords(targetPed) - GetEntityCoords(playerPed)) < 20 then
+							TaskLookAtEntity(playerPed, targetPed, 3000, 2048, 3)
+						end
+					end
+				end
+			end
+		end
+	end)
+end
